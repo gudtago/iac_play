f3dfc3117e13857a4ecfcd4d9408100a37f43ee7
gudtago <1972612+gudtago@users.noreply.github.com>
2022-08-15 04:52:02 -0400

    initial version

---
+++ main.tf
@@ -0,0 +1,40 @@
+
+provider "aws" {
+
+    region = "us-east-1"
+
+}
+
+variable "name" {
+  
+    description = "Name the instance on deploy"
+
+}
+
+resource "aws_instance" "devops_01" {
+
+  ami           = "ami-04b9e92b5572fa0d1"
+
+  instance_type = "t2.micro"
+
+  key_name      = "devops_01"
+
+
+resource "aws_instance" "devops_02" {
+
+  ami           = "ami-04b9e92b5572fa0d1"
+
+  instance_type = "t2.micro"
+
+  key_name      = "devops_02"
+
+
+  tags = {
+
+    Name = "${var.name}"
+
+  
+    }
+
+  }
+
